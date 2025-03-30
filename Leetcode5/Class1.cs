754659e886a58c1dcf14d44a8b9f6502751e344a
KadinMeyer23 <vq4717wg@go.minnstate.edu>
2025-03-30 15:29:01 -0500

    Add files via upload

---
+++ Leetcode5/Class1.cs
@@ -0,0 +1,31 @@
+﻿//Kadin Meyer
+//3/30/2025
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode5
+{
+    public class Even_Odd
+    {
+
+        public int[] Make_Even_Odd(int[]Array1) 
+        {
+            int[]Array2 = new int[Array1.Length];
+            int inc = 0;
+            int inc2 = 0;
+            for (int i = 0; i < Array1.Length; i++)
+            {
+                if (Array1[i] % 2 == 0) { Array2[inc] = Array1[i]; inc++; }
+                else { Array2[Array2.Length - inc2-1] = Array1[i]; inc2++; }
+            }
+
+
+            return Array2;
+        }
+
+    }
+}
---
+++ Leetcode5/Program.cs
@@ -0,0 +1,20 @@
+﻿//Kadin Meyer
+//3/30/2025
+
+namespace Leetcode5
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Even_Odd ae1 = new Even_Odd();
+            int[] Array1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+            int [] x = ae1.Make_Even_Odd(Array1);
+            foreach (int i in x)
+            {
+                Console.WriteLine(i);
+            }
+           
+        }
+    }
+}
